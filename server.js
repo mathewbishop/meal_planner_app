eecc5a22ac3935e23d1d7549163acdd595a8a799
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-01 11:52:49 -0500

    add views, courtesy of Landar40

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
+const path = require("path");
+const request = require("request");
 const PORT = process.env.PORT || 3000;
 
 //==============================================================
@@ -16,13 +18,30 @@ const connection = mysql.createConnection({
     database: "mealplanner_db"
 });
 
-const makeConnection = () => {
-    connection.connect(err => {
-        if (err) throw err;
-        console.log("\nConnected @ " + connection.threadId);
-    });
-}
-
+// let recipeData;
+// const callRecipeAPI = () => {
+//     let API_KEY = "484238b135b3904515a67e3f7db2eca3";
+//     let queryURL = `https://www.food2fork.com/api/search?key=${API_KEY}`
+//     request(queryURL, (err, response, body) => {
+//         if (err) throw err;
+//         if (!err && response.statusCode === 200) {
+//             recipeData = JSON.parse(body);
+//         }
+//     })
+// }
+// let weeklyPlanData;
+// const getWeeklyPlan = () => {
+//     console.log("Fetching your weekly meal plan...\n");
+//         connection.query(
+//         "SELECT meal_name FROM meals ORDER BY RAND() LIMIT 7", 
+//         (err, res) => {
+//             if (err) throw err;
+//             let data = JSON.stringify(res); 
+//             weeklyPlanData = data;
+//         }
+//     )
+    
+// }
 
 //==============================================================
 // Middleware
@@ -34,7 +53,9 @@ app.use(express.json());
 // Routes
 //==============================================================
 
-
+app.get("/", (req, res) => {
+   res.sendFile() 
+});
 
 
 
