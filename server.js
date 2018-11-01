e6eb1d2e5ac7fab49f852b792f8550c3f72b2e71
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-10-31 22:38:23 -0500

    init files

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "penguin237",
+    database: "mealplanner_db"
+});
+
+const makeConnection = () => {
+    connection.connect(err => {
+        if (err) throw err;
+        console.log("\nConnected @ " + connection.threadId);
+    });
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//==============================================================
+// Routes
+//==============================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log("Server listening on port " + PORT);
+});
