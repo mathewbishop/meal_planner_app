534a9c8b0deb7587003ca8e764d7541b7c8f0de5
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-04 15:46:13 -0600

    start csv data seed

--- data_seeds/SEED_schema_mealplanner_db.sql
+++ data_seeds/SEED_schema_mealplanner_db.sql
@@ -8,6 +8,7 @@ CREATE TABLE meals (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     meal_name VARCHAR(45) NOT NULL,
     category VARCHAR(45) NULL,
+    sub_category VARCHAR(45) NULL,
     avg_price DECIMAL(10, 2) NULL
 );
 
--- plan.md
+++ plan.md
@@ -16,6 +16,7 @@ A web app to assist in meal planning.
 4. recipe?
 5. avg price
 6. sub-cat (meat)
+7. servings
 
 
 
