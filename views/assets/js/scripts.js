a467f06756bd265bd6bfc4d653732747a433d10d
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-04 20:29:17 -0600

    update form styling

--- views/assets/js/scripts.js
+++ views/assets/js/scripts.js
@@ -64,11 +64,15 @@ $('#recipe-search-btn').click(() => {
     let API_KEY = "51c433b80e9216aebf68cc2186aaab47"
     let queryURL = `https://api.edamam.com/search?q=${searchTerm}&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=10`;
     $.get(queryURL, res => {
+        console.log(res);
         let arry = res.hits;
         arry.forEach(item => {
             console.log(item.recipe.label);
-            console.log(item.recipe.url);
-                
+            console.log(item.recipe.url);      
         })
     })
+})
+
+$('#hide-recipe-search').click(() => {
+    $('#recipe-search-div').hide();
 })
