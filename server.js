426b23216cd9a817a275089794b31fa03884361b
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2019-02-26 21:19:19 -0600

    rebuild

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 //==============================================================
 // Imports
 //==============================================================
-<<<<<<< HEAD
 require("dotenv").config()
-=======
-require("dotenv").config();
->>>>>>> f43aa8106d954c12188dbcfae446a229256f38ca
 const express = require("express");
 const apiRoutes = require("./controller/apiRoutes");
 const PORT = process.env.PORT || 3000;
