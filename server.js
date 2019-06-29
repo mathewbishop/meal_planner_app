88e47bc4109c9911203565af2cf385e2caa62e04
mathewbishop <39043451+mathewbishop@users.noreply.github.com>
2019-06-29 14:35:15 -0500

    Add PUG and change server configs.

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 //==============================================================
-// Imports
+// DEPENDENCIES & INITIALIZATION
 //==============================================================
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
-const apiRoutes = require("./controller/apiRoutes");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const apiRoutes = require("./controller/apiRoutes");
 //==============================================================
-// Middleware
+// VIEW ENGINE
+//==============================================================
+app.set("view engine", "pug");
+app.set("views", "./views");
+//==============================================================
+// MIDDLEWARE
 //==============================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("view"));
+app.use(express.static("public"));
+//==============================================================
+// ROUTES
+//==============================================================
 app.use(apiRoutes);
 //==============================================================
-// Listener
+// LISTENER
 //==============================================================
 app.listen(PORT, () => {
     console.log("Server listening on port " + PORT);
